1350faa62f2401583af01dc3fb4c0cab06e931c4
Nat <davidson@cloudflare.com>
2021-06-03 18:29:25 -0400

    WIP- attempt to fetch from ws event listener context

--- index.js
+++ index.js
@@ -3,52 +3,78 @@ import template from './template'
 let count = 0
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event))
 })
 
-async function handleSession(websocket) {
-  websocket.accept()
-  websocket.addEventListener("message", async ({ data }) => {
-    if (data === "CLICK") {
+async function handleSession(event, websocket) {
+  const error_post = new Request('$SERVER_ACCEPTING_POST', {
+    body: 'Hello from worker' + JSON.stringify(message),
+    method: 'POST',
+  })
+  await fetch(error_post)
+
+  websocket.accept().then(() => {
+    const error_post = new Request('$SERVER_ACCEPTING_POST', {
+      body: 'WEBSOCKET open: ' + data,
+      method: 'POST',
+    })
+    fetch(error_post)
+  })
+
+  websocket.addEventListener('message', evt => {
+    try {
+      const error_post = new Request('$SERVER_ACCEPTING_POST', {
+        body: 'WEBSOCKET message: ' + data,
+        method: 'POST',
+      })
+      event.waitUntil(fetch(error_post))
+    } catch (e) {
+      websocket.send(JSON.stringify(e))
+    } finally {
+      websocket.send('Fail')
+    }
+    if (data === 'CLICK') {
       count += 1
       websocket.send(JSON.stringify({ count, tz: new Date() }))
     } else {
       // An unknown message came into the server. Send back an error message
-      websocket.send(JSON.stringify({ error: "Unknown message received", tz: new Date() }))
+      websocket.send(
+        JSON.stringify({ error: 'Unknown message received', tz: new Date() }),
+      )
     }
   })
 
-  websocket.addEventListener("close", async evt => {
+  websocket.addEventListener('close', async evt => {
     // Handle when a client closes the WebSocket connection
     console.log(evt)
   })
 }
 
-const websocketHandler = async request => {
-  const upgradeHeader = request.headers.get("Upgrade")
-  if (upgradeHeader !== "websocket") {
-    return new Response("Expected websocket", { status: 400 })
+const websocketHandler = async event => {
+  const upgradeHeader = event.request.headers.get('Upgrade')
+  if (upgradeHeader !== 'websocket') {
+    return new Response('Expected websocket', { status: 400 })
   }
 
   const [client, server] = Object.values(new WebSocketPair())
-  await handleSession(server)
+  await handleSession(event, server)
 
   return new Response(null, {
     status: 101,
-    webSocket: client
+    webSocket: client,
   })
 }
 
-async function handleRequest(request) {
+async function handleRequest(event) {
   try {
-    const url = new URL(request.url)
+    const url = new URL(event.request.url)
     switch (url.pathname) {
       case '/':
         return template()
       case '/ws':
-        return websocketHandler(request)
+        return websocketHandler(event.request)
       default:
-        return new Response("Not found", { status: 404 })
+        return new Response('Not found', { status: 404 })
     }
   } catch (err) {
     return new Response(err.toString())
